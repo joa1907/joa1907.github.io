5196bdd54009e4da7a7dd9238f278fe9558a289d
joa1907 <90669163+joa1907@users.noreply.github.com>
2024-10-22 21:00:47 -0300

    light mode color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode:'class',
-	theme: {
-		extend: {
-			
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "pink-light": "#f3e7eb",
+        "purple-dark": "#774473",
+        "blue-dark": "#2d3284",
+        "blue-muted": "#455078",
+        "dark-color": "#3a425c",
+        "blue-gray": "#6b74a3",
+        "lavender-light": "#ece3f8",
+        "gray-light": "#f6f4f7",
+        "gray-muted": "#ddd1dd",
+      },
+    },
+  },
+  plugins: [],
+};
